f24b59310fb0b1671341cb7f03ab9fd9cd54885e
boggs ⚓ <6130147+b-ggs@users.noreply.github.com>
2024-05-10 00:39:58 +0800

    Initial commit

---
+++ django_template/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from django_template.home import urls as home_urls
+
+urlpatterns = [
+    path("", include(home_urls)),
+    path("django-admin/", admin.site.urls),
+]
